2f4bfd92634c42cb73aa7605d0dc0c8410f34657
Lukas St <114870613+Nympherr@users.noreply.github.com>
2023-05-30 23:05:45 +0300

    Added simple text file scraping
    Program is able to scan a file and output the result in a separate
    file. Currently URL'S are not separated and every single word is
    showed, even if there was only 1 instance of it.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <map>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
 
 std::string irasymo_pasirinkimas(){
 
@@ -24,7 +30,83 @@ std::string irasymo_pasirinkimas(){
     return ivestis;
 };
 
-void failo_nuskaitymas(){
+std::string processWord(std::string word) {
+    if (word.find("www.") != std::string::npos || 
+        word.find("http://") != std::string::npos || 
+        word.find("https://") != std::string::npos ||
+        word.find(".lt") != std::string::npos ||
+        word.find(".com") != std::string::npos ||
+        word.find(".eu") != std::string::npos) {
+        // This word looks like a URL, so don't remove any punctuation
+        return word;
+    }
+     else {
+
+        std::cout << "\n Nepakeistas zodis: " << word;
+        const std::string unwanted_chars = "„“–";
+        for (char& c : word) {
+            if (std::ispunct(c) || std::isdigit(c) || unwanted_chars.find(c) != std::string::npos) {
+                c = ' '; // replace unwanted characters with space
+            }
+        }
+        word.erase(std::remove_if(word.begin(), word.end(), ::isspace), word.end()); // remove spaces
+        std::cout << "\n pakeistas zodis: " << word << "\n\n";
+        return word;
+    }
+}
+
+std::map<std::string, int> failo_nuskaitymas(std::string failo_pavadinimas){
+
+
+    std::ifstream skaitomas_failas;
+    skaitomas_failas.open(failo_pavadinimas);
+
+    std::string nuskaityta_eilute;
+    std::map<std::string, int> pasikartojantys_zodziai;
+
+    // Skaitoma atidaryto failo kiekviena eilutė
+
+    while (std::getline(skaitomas_failas, nuskaityta_eilute)) {
+        std::istringstream iss(nuskaityta_eilute);
+        std::string word;
+
+        while (iss >> word) {
+            word = processWord(word);
+            std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+            if (!word.empty()) { // check if the word is not empty after removing spaces
+                if (pasikartojantys_zodziai.count(word) > 0) { // If the word is already in the map
+                    ++pasikartojantys_zodziai[word]; // increment its count
+                }
+                else { // If the word is not in the map
+                    pasikartojantys_zodziai[word] = 1; // add it to the map with a count of 1
+                }
+            }
+        }
+    }
+
+    skaitomas_failas.close();
+    return pasikartojantys_zodziai;
+};
+
+void lenteles_kurimas(std::map<std::string, int> duomenys){
+
+    std::ostringstream buferis;
+
+    buferis << "Žodis                                   Pasikartojimų skaičius\n";
+    buferis << "-------------------------------------------------------------------\n";
+
+    for (const auto& zodis : duomenys) {
+        buferis << std::setw(40) << std::left << zodis.first;
+        buferis << zodis.second << '\n';
+    }
+
+    const std::string& isvedimo_string = buferis.str();
+
+    std::ofstream lenteles_failas("textas.txt", std::ios_base::binary);
+    lenteles_failas.write(isvedimo_string.c_str(), isvedimo_string.size());
+    lenteles_failas.close();
+}
+void failo_tikrinimas(){
 
     // Duoda vartotojui pasirinkti failą, kurį jis nori atidaryti
 
@@ -47,7 +129,7 @@ void failo_nuskaitymas(){
     }
     catch(const char* klaidos_zinute){
         std::cerr << klaidos_zinute << std::endl;
-        std::cout << "Pabandykite dar karta\n";
+        std::cout << "Pabandykite dar kartą\n";
         std::cout << "Failo pavadinimas:   ";
         std::cin >> ivestis;
         std::cout << std::endl;
@@ -55,11 +137,22 @@ void failo_nuskaitymas(){
     }
 }
 
-    std::cout << "\n---------------- Irasomi duomenys.... ----------------\n\n";
+    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
+    std::cout << "\n---------------- Įrasomi duomenys.... ----------------\n\n";
 
-    // Kviečiam funkciją, kuri nuskaitys šį failą ir studentus sudės į bendrą vektorių.
+    // Kviečiam funkciją, kuri nuskaitys šį failą
 
-    std::cout << "Atlikta";
+    std::map pasikartojantys_zodziai = failo_nuskaitymas(failo_pavadinimas);
+
+    // Kviečiam funkciją, kuri sukurs lentelę su žodžiais ir kiek kartų pasikartojo
+    lenteles_kurimas(pasikartojantys_zodziai);
+
+    // surašomi programos veikimo laikai
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end-start;
+    std::cout << "\nVisos programos veikimo laikas: "<< diff.count() << " s\n\n";  
+    std::cout << "--------------------------------\n";
+    std::cout << "Failas sėkmingai nuskaitytas!\n\n";
 
 };
 
@@ -68,11 +161,8 @@ int main(){
 
     std::string pasirinkimas = irasymo_pasirinkimas();
     if(pasirinkimas == "1"){
-        failo_nuskaitymas();
+        failo_tikrinimas();
     }
-
-
-
 
     return 0;
 }
